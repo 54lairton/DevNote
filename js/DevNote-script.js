bf5dd861a56156fbde0fe9163d40c8020cd65c9a
Marks <108982871+54lairton@users.noreply.github.com>
2022-12-14 10:05:11 -0300

    Add files via upload

--- js/DevNote-script.js
+++ js/DevNote-script.js
@@ -12,11 +12,32 @@ const valueTitle = document.getElementById("add-title-steps-text");
 
 const title_list = [...document.querySelectorAll(".title-list")];
 
+const DELETE_PROGRESS = document.getElementById("delete-progress");
+
+const PROGRESS = document.getElementById("progress-retire");
+const CONT_PROGRESS = document.getElementById("cont-progress");
+
+PROGRESS.addEventListener("click", ()=>{
+    CONT_PROGRESS.style.display = 'none';
+})
+DELETE_PROGRESS.addEventListener("click", (e)=>{
+    window.localStorage.clear();
+    DELETE_PROGRESS.style.display = "none";
+    setTimeout(() => {
+        location.href = location.href;
+    }, 1000);
+})
+const perms = document.getElementById("perms");
 var array=[];
 var array_2=[];
 var color;
 window.onload = ()=>{
+    CONT_PROGRESS.style.display = 'none';
     if(window.localStorage.TitleSteps){
+        CONT_PROGRESS.style.display = 'grid';
+        DELETE_PROGRESS.style.display = "block";
+        perms.style.display = 'block';
+        createTitle.setAttribute("disabled", "");
         div = document.createElement("div");
         div.setAttribute('class', classes.list);
         div.setAttribute('id', "list-Type");
@@ -27,8 +48,13 @@ window.onload = ()=>{
             div.appendChild(h1);
         }
         steps.appendChild(div);
+    }else{
+        perms.style.display = 'none';
+        DELETE_PROGRESS.style.display = "none";
     }
     if(window.localStorage.Steps){
+        CONT_PROGRESS.style.display = 'grid';
+        DELETE_PROGRESS.style.display = "block";
         div = document.createElement("div");
         div.setAttribute('class', classes.list);
         div.setAttribute('id', "list-Type");
@@ -92,19 +118,19 @@ theme.addEventListener("click", ()=>{
         window.document.body.style.background = "rgb(25, 25, 25)";
         title_page.style.color = "white";
         window.document.body.style.color = "white";
-
+        
         how.style.backgroundColor = "black";
         how.style.color = "rgb(158, 208, 208)";
         textInput.map((e)=>{
             e.style.border = "1px solid rgba(255, 255, 255, 0.459)";
             e.style.color = "white";
         })
-
     }
     else if(theme.innerHTML == "🌙"){
         saved.style.display = "block";
         color = "white";
         theme.innerHTML = "🌞";
+        title_page.style.color = "black";
         textInput.map((e)=>{
             e.style.border = "1px solid rgba(0, 0, 0, 0.459)";
             e.style.color = "black";
@@ -114,7 +140,6 @@ theme.addEventListener("click", ()=>{
         window.document.body.style.color = "black";
         how.style.backgroundColor = "rgb(252, 252, 252) ";
         how.style.color = "darkslategrey";
-
     }
     window.localStorage.setItem("Bg",color);
     setTimeout(()=>{
@@ -129,12 +154,11 @@ const classes = {
     titleList: "title-list",
     listOptions: "list-options",
     input:  {
-
         id: "check"
     }
 }
 var div, h1, input, label;
-createTitle.addEventListener("click", ()=>{
+createTitle.addEventListener("click", (evt)=>{
     var title = valueTitle.value;
     array.push(title);
     window.localStorage.setItem("TitleSteps", JSON.stringify(array));
@@ -144,12 +168,17 @@ createTitle.addEventListener("click", ()=>{
         setTimeout(()=>{
             messageError.style.display = "none";
         },5000)
+        perms.style.display = 'none';
+        evt.target.removeAttribute("disabled", "");
     }else{
         Title();
+        DELETE_PROGRESS.style.display = "block";
+        perms.style.display = 'block';
         valueTitle.value = '';
+        evt.target.setAttribute("disabled", "");
     }
 })
-create.addEventListener("click", ()=>{
+create.addEventListener("click", (e)=>{
     array_2.push(valueSteps.value)
     window.localStorage.setItem("Steps",JSON.stringify(array_2));
     if(!valueSteps.value){
